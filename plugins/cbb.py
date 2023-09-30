302655409f125a209d08fa52922c5a2bfda944db
bigabids <146448655+bigabids@users.noreply.github.com>
2023-09-30 22:13:59 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/cyber_Robots'>Click here</a>\n○ Channel : @hentai_Providerss\n○ Support Group : @animeandmotivation</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://t.me/Red_hair_shanks_69'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/Red_hair_shanks_69'>Click here</a>\n○ Channel : @Red_hair_shanks_69\n○ Support Group : @Red_hair_shanks_69</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
